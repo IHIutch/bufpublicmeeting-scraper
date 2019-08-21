ba175b8b05b36998b61424537ad8a16693a6f0a2
IHIutch <jbhutch01@gmail.com>
2019-08-20 22:42:02 -0400

    Starting a scrapy project

---
+++ scraper.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class MeetingsSpider(scrapy.Spider):
+    name = "meetings"
+    start_urls = [
+        'http://buffalony.iqm2.com/Citizens/Calendar.aspx'
+    ]
+
+    def parse(self, response):
+        for meeting in response.css('div.MeetingRow'):
+            yield {
+                'date': meeting.css('div.RowTop div.RowLink a::text').get(),
+                'subject': meeting.css('div.RowBottom div.RowDetails::text').get(),
+                'links': meeting.css('div.MeetingLinks div a::text').getall(),
+            }
