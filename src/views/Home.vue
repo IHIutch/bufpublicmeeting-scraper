1084ce721d715d86b2fc7635437c4a86fe55e2be
IHIutch <jbhutch01@gmail.com>
2019-09-07 00:48:23 -0400

    Add vue, bootstrap vue to render data

---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col cols="3"> </b-col>
+        <b-col cols="8">
+          <div
+            class="border rounded p-4 mb-4"
+            v-for="(meeting, idx) in meetings"
+            :key="idx"
+          >
+            <div class="d-flex">
+              <div>
+                <h1 class="h3">{{ meeting.subject }}</h1>
+              </div>
+            </div>
+            <div class="d-flex mb-3">
+              <div>
+                <span class="text-muted font-weight-bold">
+                  {{ meeting.date }}
+                </span>
+              </div>
+              <div
+                class="d-flex align-items-center justify-content-end flex-grow-1"
+              >
+                <a
+                  href="#"
+                  class="d-inline-flex px-2"
+                  v-for="(link, idx) in meeting.links"
+                  :key="idx"
+                >
+                  {{ link }}
+                </a>
+              </div>
+            </div>
+            <p v-html="meeting.details"></p>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Meetings from "@/data/meetings.json";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      meetings: Meetings
+    };
+  }
+};
+</script>
