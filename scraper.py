ad55c01c081a86b9135f318ca2df0d091ad88833
IHIutch <jbhutch01@gmail.com>
2019-10-12 14:42:07 -0400

    Some updates

--- scraper.py
+++ scraper.py
@@ -1,18 +1,57 @@
 import scrapy
+import logging
+
+from scrapy.utils.log import configure_logging
+
 
 class MeetingsSpider(scrapy.Spider):
+    configure_logging(install_root_handler=False)
+    logging.basicConfig(
+        filename='log.txt',
+        format='%(levelname)s: %(message)s',
+        level=logging.INFO
+    )
+
     name = "meetings"
     start_urls = [
         'http://buffalony.iqm2.com/Citizens/Calendar.aspx'
     ]
 
+    def parse_meeting(self, response):
+        meetingTitle = response.css(
+            'span#ContentPlaceholder1_lblMeetingGroup::text').get()
+
+        return {'meetingTitle': meetingTitle}
+
     def parse(self, response):
         for meeting in response.css('div.MeetingRow'):
+            links = []
+            meetingLink = meeting.css(
+                'div.RowTop div.RowLink a::attr(href)').get()
+            date = meeting.css('div.RowTop div.RowLink a::text').get()
+            details = meeting.css(
+                'div.RowTop div.RowLink a::attr(title)').get()
+            subject = meeting.css('div.RowBottom div.RowDetails::text').get()
+            linkText = meeting.css(
+                'div.MeetingLinks div a::text').getall()
+            linkHref = meeting.css(
+                'div.MeetingLinks div a::attr(href)').getall()
+
+            links.append({
+                'text': linkText,
+                'link': linkHref
+            })
+
+            if meetingLink is not None:
+                yield response.follow(
+                    meetingLink, callback=self.parse_meeting)
+
             yield {
-                'date': meeting.css('div.RowTop div.RowLink a::text').get(),
-                'details': meeting.css('div.RowTop div.RowLink a::attr(title)').get(),
-                'subject': meeting.css('div.RowBottom div.RowDetails::text').get(),
-                'links': meeting.css('div.MeetingLinks div a::text').getall(),
+                'meetingLink': meetingLink,
+                'date': date,
+                'details': details,
+                'subject': subject,
+                'links': links,
             }
 
-# Run with `scrapy runspider scraper.py -o src/data/meetings.json` in the terminal
+# Run with `scrapy runspider scraper.py -t json -o - > src/data/meetings.json` in the terminal
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,39 +1,34 @@
 <template>
   <Layout>
-
-              <div
-            class="border rounded p-4 mb-4"
-            v-for="(meeting, idx) in meetings"
+    <div
+      class="border rounded p-4 mb-4"
+      v-for="(meeting, idx) in meetings"
+      :key="idx"
+    >
+      <div class="flex">
+        <div>
+          <h1 class="h3">{{ meeting.subject }}</h1>
+        </div>
+      </div>
+      <div class="flex mb-3">
+        <div>
+          <span class="text-muted font-weight-bold">
+            {{ meeting.date }}
+          </span>
+        </div>
+        <div class="d-flex align-items-center justify-content-end flex-grow-1">
+          <a
+            href="#"
+            class="inline-flex px-2"
+            v-for="(link, idx) in meeting.links"
             :key="idx"
           >
-            <div class="flex">
-              <div>
-                <h1 class="h3">{{ meeting.subject }}</h1>
-              </div>
-            </div>
-            <div class="flex mb-3">
-              <div>
-                <span class="text-muted font-weight-bold">
-                  {{ meeting.date }}
-                </span>
-              </div>
-              <div
-                class="d-flex align-items-center justify-content-end flex-grow-1"
-              >
-                <a
-                  href="#"
-                  class="inline-flex px-2"
-                  v-for="(link, idx) in meeting.links"
-                  :key="idx"
-                >
-                  {{ link }}
-                </a>
-              </div>
-            </div>
-            <p v-html="meeting.details"></p>
-          </div>
-
-
+            {{ link }}
+          </a>
+        </div>
+      </div>
+      <p v-html="meeting.details"></p>
+    </div>
   </Layout>
 </template>
 
@@ -42,13 +37,12 @@ import Meetings from "@/data/meetings.json";
 
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Hello, world!"
   },
-    data() {
+  data() {
     return {
       meetings: Meetings
     };
   }
-
-}
+};
 </script>
