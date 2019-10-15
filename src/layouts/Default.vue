2e8b4ff61be5d9199b0ea1a422b3130190874d8d
IHIutch <jbhutch01@gmail.com>
2019-10-15 08:33:55 -0400

    Create filter list

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,61 +1,6 @@
 <template>
   <div class="flex container mx-auto">
-    <div class="w-1/4 px-4">
-      <div class="sticky top-0 pt-4">
-        <h2 class="font-medium text-2xl mb-2">
-          Sorting &amp; Filters
-        </h2>
-        <div class="border rounded p-2 mb-4">
-          <fieldset>
-            <legend class="font-medium text-xl">Sort by:</legend>
-            <div>
-              <input type="radio" name="sorting" />
-              <label>Meeting Name (Asc)</label>
-            </div>
-            <div>
-              <input type="radio" name="sorting" />
-              <label>Meeting Name (Desc)</label>
-            </div>
-            <div>
-              <input type="radio" name="sorting" />
-              <label>Meeting Date (Asc)</label>
-            </div>
-            <div>
-              <input type="radio" name="sorting" />
-              <label>Meeting Date (Desc)</label>
-            </div>
-          </fieldset>
-        </div>
-        <div class="border rounded p-2">
-          <fieldset>
-            <legend class="font-medium text-xl">Filter Meeting Type:</legend>
-            <div>
-              <input
-                type="checkbox"
-                name="filtering"
-                id="legislation_committee"
-              />
-              <label for="legislation_committee">Legislation Committee</label>
-            </div>
-            <div>
-              <input type="checkbox" name="filtering" />
-              <label>Preservation Board</label>
-            </div>
-            <div>
-              <input type="checkbox" name="filtering" />
-              <label>Bicycle and Pedestrian Advisory Board</label>
-            </div>
-            <div>
-              <input type="checkbox" name="filtering" />
-              <label>Common Council</label>
-            </div>
-          </fieldset>
-        </div>
-      </div>
-    </div>
-    <div class="w-3/4 px-4">
-      <slot />
-    </div>
+    <slot />
   </div>
 </template>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,39 +1,83 @@
 <template>
   <Layout>
-    <main v-for="(meeting, idx) in meetings" :key="idx">
-      <template v-if="moment() < moment(meeting.date)">
-        <div class="border rounded p-4 mb-4">
-          <div class="flex">
-            <h2 class="text-2xl font-medium">
-              <g-link :to="meeting.meetingId" class="hover:underline">
-                {{ meeting.meetingType }}
-              </g-link>
-            </h2>
-          </div>
-          <div class="flex mb-3">
+    <aside class="w-1/4">
+      <div class="sticky top-0 pt-4">
+        <h2 class="font-medium text-2xl mb-2">
+          Sorting &amp; Filters
+        </h2>
+        <div class="border rounded p-2 mb-4">
+          <fieldset>
+            <legend class="font-medium text-xl">Sort by:</legend>
             <div>
-              <span class="text-muted font-weight-bold">
-                {{ meeting.date | moment }}
-              </span>
+              <input type="radio" name="sorting" />
+              <label>Meeting Name (Asc)</label>
             </div>
-          </div>
-          <div>
-            <div
-              class="d-flex align-items-center justify-content-end flex-grow-1"
-            >
-              <a
-                :href="link.linkUrl"
-                class="inline-flex px-2 text-teal-700 hover:underline"
-                v-for="(link, idx) in meeting.links"
-                :key="idx"
-              >
-                {{ link.linkText }}
-              </a>
+            <div>
+              <input type="radio" name="sorting" />
+              <label>Meeting Name (Desc)</label>
             </div>
-          </div>
-          <p v-html="meeting.details"></p>
+            <div>
+              <input type="radio" name="sorting" />
+              <label>Meeting Date (Asc)</label>
+            </div>
+            <div>
+              <input type="radio" name="sorting" />
+              <label>Meeting Date (Desc)</label>
+            </div>
+          </fieldset>
         </div>
-      </template>
+        <div class="border rounded p-2">
+          <fieldset>
+            <legend class="font-medium text-xl">Filter Meeting Type:</legend>
+            <div v-for="(types, index) in filterTypes" :key="index">
+              <input type="checkbox" name="filtering" :id="index" />
+              <label :for="index">
+                {{ filterTypes[index][0].meetingType }}
+              </label>
+              <span class="text-white bg-blue-500 p-1 rounded">{{
+                filterTypes[index].length
+              }}</span>
+            </div>
+          </fieldset>
+        </div>
+      </div>
+    </aside>
+    <main class="w-3/4">
+      <div v-for="(meeting, idx) in meetings" :key="idx">
+        <template v-if="moment() < moment(meeting.date)">
+          <div class="border rounded p-4 mb-4">
+            <div class="flex">
+              <h2 class="text-2xl font-medium">
+                <g-link :to="meeting.meetingId" class="hover:underline">
+                  {{ meeting.meetingType }}
+                </g-link>
+              </h2>
+            </div>
+            <div class="flex mb-3">
+              <div>
+                <span class="text-muted font-weight-bold">
+                  {{ meeting.date | moment }}
+                </span>
+              </div>
+            </div>
+            <div>
+              <div
+                class="d-flex align-items-center justify-content-end flex-grow-1"
+              >
+                <a
+                  :href="link.linkUrl"
+                  class="inline-flex px-2 text-teal-700 hover:underline"
+                  v-for="(link, idx) in meeting.links"
+                  :key="idx"
+                >
+                  {{ link.linkText }}
+                </a>
+              </div>
+            </div>
+            <p v-html="meeting.details"></p>
+          </div>
+        </template>
+      </div>
     </main>
   </Layout>
 </template>
@@ -63,6 +107,27 @@ export default {
       // console.log(meeting[key]);
       this.meetings.push(meeting[key]);
     });
+  },
+  computed: {
+    filterTypes() {
+      let obj = {};
+      this.meetings.forEach(meeting => {
+        let type = meeting.meetingType;
+        type = type
+          .split(" - ")
+          .join("-")
+          .toLowerCase();
+        type = type.split(" ").join("-");
+
+        if (Object.keys(obj).includes(type)) {
+          obj[type].push(meeting);
+        } else {
+          obj[type] = [];
+          obj[type].push(meeting);
+        }
+      });
+      return obj;
+    }
   },
   filters: {
     moment(value) {
