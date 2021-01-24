5a94b7bf7ffb0c8b53e9b97b7c0fa9f2a79fad51
Jonathan Hutchison <jh@helmux.com>
2021-01-24 11:26:13 -0500

    tailwind updates

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,35 +3,31 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-const tailwind = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const purgecss = require("@fullhuman/postcss-purgecss");
 
-const postcssPlugins = [tailwind, autoprefixer];
-
-if (process.env.NODE_ENV === "production") postcssPlugins.push(purgecss());
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
-  siteName: "BufPublicMeetings",
-  siteUrl: "http://openmeetings.jbhutch.com",
+  siteName: 'BufPublicMeetings',
+  siteUrl: 'http://openmeetings.jbhutch.com',
   templates: {
     Meeting: [
       {
-        path: "/:meetingGroupUrlify/:meetingTypeUrlify/:meetingId",
-        component: "./src/templates/MeetingTemplate.vue",
+        path: '/:meetingGroupUrlify/:meetingTypeUrlify/:meetingId',
+        component: './src/templates/MeetingTemplate.vue',
       },
     ],
   },
   plugins: [
     {
-      use: "@gridsome/plugin-sitemap",
+      use: '@gridsome/plugin-sitemap',
     },
   ],
   css: {
     loaderOptions: {
       postcss: {
-        plugins: postcssPlugins,
+        plugins: [tailwindcss, autoprefixer],
       },
     },
   },
-};
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ['./src/**/*.{vue,js}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  whitelist: [
+    'body',
+    'html',
+    'img',
+    'a',
+    'g-image',
+    'g-image--lazy',
+    'g-image--loaded',
+  ],
+}
