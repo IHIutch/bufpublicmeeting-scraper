51ba127a847c4c80fa7f9389616ef889dab051e8
Jonathan Hutchison <jh@helmux.com>
2021-01-26 00:01:25 -0500

    Fix layout casing

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import '~/assets/scss/main.scss'
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from '~/layouts/default.vue'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
